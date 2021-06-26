e8264f3ec9d21e0cea6840a3143dff4ea5bfd847
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2021-06-26 22:20:57 +0200

    v0.2.2.8
    Trying to implement GSAP in the grid repositioning

--- src/Components/Areas/Content/Product/Product.jsx
+++ src/Components/Areas/Content/Product/Product.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import {selectProductAreaResponsive, setGridPASmall, setGridPAMid } from "../../../../features/productAreaResponsive/productAreaResponsiveSlice";
 import { useDispatch } from "react-redux";
 import styled from "styled-components"
+import { useEffect } from "react";
 
 const PRODUCTAREA = styled.div`
   position: relative;
@@ -11,33 +12,40 @@ const PRODUCTAREA = styled.div`
   background: blueviolet;
   display: grid;
   grid-auto-flow: dense;
-  grid-template-columns: ${props => props.gridTC};
+  grid-template-columns: 1fr 1fr;
   grid-auto-rows: 55px;
+  @media screen and (min-width: 800px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  @media only screen and (min-width: 1251px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
 `
 
 export default function Product({ children }) {
-  const gridPA = useSelector(selectProductAreaResponsive).gridPA
-  const dispatch = useDispatch()
-  console.log(gridPA);
+  // const gridPA = useSelector(selectProductAreaResponsive).gridPA
+  // const dispatch = useDispatch()
+  // console.log(gridPA);
 
-  const mediaQuery = '(max-width: 867px)';
-  const mediaQueryList = window.matchMedia(mediaQuery)
-  if (mediaQueryList.matches) {
-    dispatch(setGridPASmall())
-  } else {
-    dispatch(setGridPAMid())
-  }
-  mediaQueryList.addEventListener('change', event => {
-    if (event.matches) {
-      dispatch(setGridPASmall())
-    } else {
-      dispatch(setGridPAMid())
-    }
-  })
+  // const mediaQuery = '(max-width: 867px)';
+  // const mediaQueryList = window.matchMedia(mediaQuery)
+  // if (mediaQueryList.matches) {
+  //   dispatch(setGridPASmall())
+  // } else {
+  //   dispatch(setGridPAMid())
+  // }
+  // mediaQueryList.addEventListener('change', event => {
+  //   if (event.matches) {
+  //     dispatch(setGridPASmall())
+  //   } else {
+  //     dispatch(setGridPAMid())
+  //   }
+  // })
 
+  
 
   return (
-    <PRODUCTAREA id='productarea' gridTC={gridPA}>
+    <PRODUCTAREA id='productarea' >
       {children}
     </PRODUCTAREA>
   )
--- src/Components/Products/ProductTrack.jsx
+++ src/Components/Products/ProductTrack.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import TrkLogoThumbNail from "./Thumbnails/TrkLogoThumbnail"
+
 
 export default function ProductTrack({ trackID, title, artist, artwork }) {
   const [tracksStreamLink, setTracksStreamLink] = useState()
+  var playlistTrack = useRef(null)
+
+  useEffect(() => {
+    console.log(playlistTrack);
+  }, [])
 
   setTimeout(() => {
     // clickHandle()
@@ -19,22 +25,25 @@ export default function ProductTrack({ trackID, title, artist, artwork }) {
   const gridAreaInA = ' "logo _title _artist _tags badge1 likebar" "logo title artist tags badge2 likebar"'
 
   return (
-    <div id='productTrack' style={{
-      height: '97%',
-      width: '97%',
-      maxWidth: '600px',
-      gridRow: 'span 2',
-      gridColumn: 'span 2',
-      alignSelf: 'center',
-      justifySelf: 'center'
-    }}>
+    <div
+      ref={l => playlistTrack = l }
+      style={{
+        height: '97%',
+        width: '97%',
+        maxWidth: '600px',
+        gridRow: 'span 2',
+        gridColumn: 'span 2',
+        alignSelf: 'center',
+        justifySelf: 'center',
+        transition: 'ease 0.5s',
+      }}>
       <div id='productTrackGrid' style={{
         height: '100%',
         width: '100%',
         background: '#1C4A72',
         border: '1px solid inset rgb(13, 46, 65)',
         fontSize: '75%',
-        borderRadius: '5px',        
+        borderRadius: '5px',
         justifySelf: 'center',
         display: 'grid',
         gridTemplateColumns: `${gridColInA}`,
@@ -42,11 +51,11 @@ export default function ProductTrack({ trackID, title, artist, artwork }) {
         gridTemplateAreas: `${gridAreaInA}`
       }}>
         <TrkLogoThumbNail coverURL={artwork}></TrkLogoThumbNail>
-        <div style={{ gridArea: '_title', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize:'90%' }}>Track Title</div>
+        <div style={{ gridArea: '_title', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize: '90%' }}>Track Title</div>
         <div style={{ gridArea: 'title', padding: '1px', color: '#EEF4FA' }}>{title}</div>
-        <div style={{ gridArea: '_artist', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize:'90%' }}>Artist</div>
+        <div style={{ gridArea: '_artist', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize: '90%' }}>Artist</div>
         <div style={{ gridArea: 'artist', padding: '1px', color: '#EEF4FA' }}>{artist}</div>
-        <div style={{ gridArea: '_tags', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize:'90%' }}>Tags/Style</div>
+        <div style={{ gridArea: '_tags', alignSelf: 'end', padding: '1px', color: '#03090E', fontSize: '90%' }}>Tags/Style</div>
         <div style={{ gridArea: 'tags', padding: '1px', color: '#EEF4FA' }}>#tekno</div>
         <div style={{ gridArea: 'badge1', background: 'rgb(13, 46, 65)', borderRadius: '100%', height: '74%', width: '74%', placeSelf: 'center' }}></div>
         <div style={{ background: 'rgb(13, 46, 65, 0.167)', gridArea: 'likebar', display: 'grid' }}>
