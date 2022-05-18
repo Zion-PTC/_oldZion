5f7a12a9124729be651903e4990a2577517599fe
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2022-05-18 21:51:06 +0200

    replace history 2/2

---
+++ packages/@zionrepack/history/index.ts
@@ -0,0 +1,9 @@
+import def from 'history';
+
+export type Action = def.Action;
+
+export let createBrowserHistory = def.createBrowserHistory;
+export let createHashHistory = def.createHashHistory;
+export let createMemoryHistory = def.createMemoryHistory;
+export let createPath = def.createPath;
+export let parsePath = def.parsePath;
