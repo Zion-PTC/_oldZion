61c427e9691b6d8e24ce46433df67196b6a6ef47
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2022-05-18 20:39:45 +0200

    tests

--- packages/@zionstate_node/fs/index.ts
+++ packages/@zionstate_node/fs/index.ts
@@ -1,1 +1,8 @@
-// TODO inizializzare Repo
+// TODO #1 inizializzare Repo
+// prova variazione
+
+// apparentemente quando viene clonato un repo al di fuori
+// del Monorepo, tramite Fork, e in seguito questa cartella
+// viene spostata all'interno del Monorepo, sembrerebbe che
+// a quel punto non venga piu visualizzato come un file
+// senza estenzione bensì vengono mostrate tutte le cartelle.
