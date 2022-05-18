a5b88c263b032d2a7136884eee84ca03582c8899
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2022-05-18 21:10:26 +0200

    fontawesome

---
+++ packages/@zionrepack/fortawesome-fontawesome-svg-core/index.ts
@@ -0,0 +1,68 @@
+import {
+  AbstractElement as fscAbstractElement,
+  Attributes as fscAttributes,
+  DOM as fscDOM,
+  FaSymbol as fscFaSymbol,
+  FlipProp as fscFlipProp,
+  FontawesomeObject as fscFontawesomeObject,
+  Icon as fscIcon,
+  IconDefinition as fscIconDefinition,
+  IconLookup as fscIconLookup,
+  IconName as fscIconName,
+  IconPack as fscIconPack,
+  IconParams as fscIconParams,
+  IconPathData as fscIconPathData,
+  IconPrefix as fscIconPrefix,
+  IconProp as fscIconProp,
+  Layer as fscLayer,
+  LayerParams as fscLayerParams,
+  Library as fscLibrary,
+  Params as fscParams,
+  PullProp as fscPullProp,
+  RotateProp as fscRotateProp,
+  SizeProp as fscSizeProp,
+  Styles as fscStyles,
+  Text as fscText,
+  TextParams as fscTextParams,
+  Transform as fscTransform,
+} from '@fortawesome/fontawesome-svg-core';
+import * as fsc from '@fortawesome/fontawesome-svg-core';
+
+export type AbstractElement = fscAbstractElement;
+export type Attributes = fscAttributes;
+export type DOM = fscDOM;
+export type FaSymbol = fscFaSymbol;
+export type FlipProp = fscFlipProp;
+export type FontawesomeObject = fscFontawesomeObject;
+export type Icon = fscIcon;
+export type IconDefinition = fscIconDefinition;
+export type IconLookup = fscIconLookup;
+export type IconName = fscIconName;
+export type IconPack = fscIconPack;
+export type IconParams = fscIconParams;
+export type IconPathData = fscIconPathData;
+export type IconPrefix = fscIconPrefix;
+export type IconProp = fscIconProp;
+export type Layer = fscLayer;
+export type LayerParams = fscLayerParams;
+export type Library = fscLibrary;
+export type Params = fscParams;
+export type PullProp = fscPullProp;
+export type RotateProp = fscRotateProp;
+export type SizeProp = fscSizeProp;
+export type Styles = fscStyles;
+export type Text = fscText;
+export type TextParams = fscTextParams;
+export type Transform = fscTransform;
+
+export let config = fsc.config;
+export let counter = fsc.counter;
+export let dom = fsc.dom;
+export let findIconDefinition = fsc.findIconDefinition;
+export let icon = fsc.icon;
+export let layer = fsc.layer;
+export let library = fsc.library;
+export let noAuto = fsc.noAuto;
+export let parse = fsc.parse;
+export let text = fsc.text;
+export let toHtml = fsc.toHtml;
