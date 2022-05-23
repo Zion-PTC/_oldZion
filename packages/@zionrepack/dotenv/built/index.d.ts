cc92bb713e81709d7407911ed89c898d2492d3dc
Giacomo <WAW@WAW.homenet.telecomitalia.it>
2022-05-23 20:19:59 +0200

    v 0.0.2

--- packages/@zionrepack/dotenv/built/index.d.ts
+++ packages/@zionrepack/dotenv/built/index.d.ts
@@ -1,10 +1,11 @@
-import * as dotenv from 'dotenv';
-import { DotenvConfigOptions, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
-export interface IDotenvConfigOptions extends DotenvConfigOptions {
-}
-export interface IDotenvConfigOutput extends DotenvConfigOutput {
-}
-export interface IDotenvParseOutput extends DotenvParseOutput {
-}
+import * as dotenv from '@zionrepack/dotenv';
+import {
+  DotenvConfigOptions,
+  DotenvConfigOutput,
+  DotenvParseOutput,
+} from '@zionrepack/dotenv';
+export interface IDotenvConfigOptions extends DotenvConfigOptions {}
+export interface IDotenvConfigOutput extends DotenvConfigOutput {}
+export interface IDotenvParseOutput extends DotenvParseOutput {}
 export declare let config: typeof dotenv.config;
 export declare let parse: typeof dotenv.parse;
