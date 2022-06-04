40e85ee04ad64dd0c0ff1abbae3dc0549ae35b42
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-06-05 00:41:43 +0200

    preparato per inserire custom styles

--- packages/@zionrepack/styled-components/built/index.d.ts
+++ packages/@zionrepack/styled-components/built/index.d.ts
@@ -1,15 +1,1 @@
-/// <reference types="react" />
-import * as styledAsterisk from "styled-components";
-import styled from "styled-components";
-export default styled;
-export declare let ServerStyleSheet: typeof styledAsterisk.ServerStyleSheet;
-export declare let StyleSheetManager: typeof styledAsterisk.StyleSheetManager;
-export declare let ThemeConsumer: import("react").Consumer<any>;
-export declare let ThemeContext: import("react").Context<any>;
-export declare let ThemeProvider: styledAsterisk.ThemeProviderComponent<any, any>;
-export declare let createGlobalStyle: typeof styledAsterisk.createGlobalStyle;
-export declare let css: styledAsterisk.ThemedCssFunction<styledAsterisk.DefaultTheme>;
-export declare let isStyledComponent: typeof styledAsterisk.isStyledComponent;
-export declare let keyframes: typeof styledAsterisk.keyframes;
-export declare let useTheme: typeof styledAsterisk.useTheme;
-export declare let withTheme: styledAsterisk.WithThemeFnInterface<styledAsterisk.DefaultTheme>;
+export {};
--- packages/@zionrepack/styled-components/built/index.js
+++ packages/@zionrepack/styled-components/built/index.js
@@ -1,15 +1,1 @@
-import * as styledAsterisk from "styled-components";
-import styled from "styled-components";
-export default styled;
-let { ServerStyleSheet: sss, StyleSheetManager: ssm, ThemeConsumer: tc, ThemeContext: tc1, ThemeProvider: tp, createGlobalStyle: cg, css: cs, isStyledComponent: isc, keyframes: k, useTheme: u, withTheme: w, } = styledAsterisk;
-export let ServerStyleSheet = sss;
-export let StyleSheetManager = ssm;
-export let ThemeConsumer = tc;
-export let ThemeContext = tc1;
-export let ThemeProvider = tp;
-export let createGlobalStyle = cg;
-export let css = cs;
-export let isStyledComponent = isc;
-export let keyframes = k;
-export let useTheme = u;
-export let withTheme = w;
+export {};
