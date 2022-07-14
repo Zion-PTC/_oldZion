698a9086a27b1f0eaafab54e0e7580947b3e0ec8
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-07-14 13:06:22 +0200

    upate and created export

--- packages/@zionstate_node/fs-utilities/index.ts
+++ packages/@zionstate_node/fs-utilities/index.ts
@@ -1,9 +1,1 @@
-// TODO #1 inizializzare
-
-// Prova cambiamenti
-
-// In questo repo invece il metodo di import in differita
-// non sembra aver funzionato.
-
-// riprovo
-// all seconda volta ha funzionato
+export {};
