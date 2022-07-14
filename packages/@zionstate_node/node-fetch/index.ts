7fc75fa245ec4d9eb7c258da0416c6324c58f183
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-07-14 13:06:30 +0200

    update

--- packages/@zionstate_node/node-fetch/index.ts
+++ packages/@zionstate_node/node-fetch/index.ts
@@ -1,10 +1,10 @@
-import { ZionResponse as ZR, IZionResponse as IZR } from './src/Response.js';
+import { ZionResponse as ZR, IZionResponse as IZR } from "./src/Response.js";
 import {
   ZionRequest as ZReq,
   IZionRequest as IZReq,
   ZionRequestInfo as ZReqInfo,
   ZionRequestInit as ZReqInit,
-} from './src/Request.js';
+} from "./src/Request.js";
 
 export let ZionResponse = ZR;
 export type IZionResponse = IZR;
