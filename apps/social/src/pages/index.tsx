8f778eeb387442b08497ff570176ab38911530e8
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-09-09 14:00:16 +0200

    Creata navigazione base di app social Zion

---
+++ apps/social/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import Layout from "../components/layout";
+
+export default function Landing() {
+  return (
+    <Layout home>
+      <h1>Landing</h1>
+      <Link href="/auth/login">login</Link>
+      <br />
+      <Link href="/auth/signup">signup</Link>
+      <br />
+      <Link href="/auth/connectwallet">connect wallet</Link>
+    </Layout>
+  );
+}
