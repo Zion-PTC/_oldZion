5d05a27e2fce72708e125f40df912d761d9b469c
ariannatnl <arianna.minucci@me.com>
2022-09-13 17:07:05 +0200

    add ThemeProvider to app and create basic dark ad light theme. @ariannatnl Fixes #14

--- apps/pfp_minter/src/pages/collection/index.tsx
+++ apps/pfp_minter/src/pages/collection/index.tsx
@@ -4,7 +4,7 @@ import Card from "../../components/Card/Card";
 import Layout from "../../components/layout";
 import { getStatic } from "../../lib/static/getStatic";
 import { Data, NftData, NftsData } from "../api/types";
-import underlord1 from "/Users/WAW/Documents/Projects/ZION/apps/pfp_minter/database/batch1/pfp3_65_740512378.jpg";
+import underlord1 from "../../../database/batch1/pfp3_65_740512378.jpg";
 
 const { getStaticProps: gst } = getStatic<NftData, NftsData>({
   type: "nft",
