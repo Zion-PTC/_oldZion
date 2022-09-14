bb0cc7c40e9b245629dab9e1fdbc489b98725e84
ariannatnl <arianna.minucci@me.com>
2022-09-14 13:54:32 +0200

    aggiuto theme provider ( su cell non finziona) da capire magari creare issue
    Fixes #29

---
+++ apps/zion/src/components/theme.tsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+type Theme = { body: string; textColor: string; headingColor: string };
+export const darkTheme: Theme = {
+  body: "#000",
+  textColor: "#fff",
+  headingColor: "lightblue",
+};
+export const lightTheme: Theme = {
+  body: "#fff",
+  textColor: "#000",
+  headingColor: "#d23669",
+};
+export const GlobalStyles = createGlobalStyle<{
+  theme: Theme;
+}>`
+  body {
+    background: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.textColor};
+    transition: .3s ease;
+    margin:0;
+    box-sizing: border-box;
+  }
+  h2{
+    color: ${(props) => props.theme.headingColor};
+  }
+  main{
+    height:100%;
+  }
+`;
