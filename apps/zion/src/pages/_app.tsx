48aedc8a1198d67447da33e8824c020a5853fdb4
ariannatnl <arianna.minucci@me.com>
2022-09-15 09:02:54 +0200

    footer

--- apps/zion/src/pages/_app.tsx
+++ apps/zion/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme, GlobalStyles } from "../components/theme";
 
@@ -25,15 +25,18 @@ function Application({ Component, pageProps }) {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
+  const [appHeight, setAppHeight] = useState(0);
+  const app = useRef<HTMLDivElement>();
+
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <App>
+      <App ref={app} height={appHeight}>
         <NavBar>
           <button onClick={switchTheme}>Switch Theme</button>
         </NavBar>
         <Component {...pageProps} />
-        <Footer>ciao</Footer>
+        <Footer>ciao sono il footer e devo stare in basso</Footer>
       </App>
     </ThemeProvider>
   );
